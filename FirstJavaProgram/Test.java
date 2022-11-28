413fcb3deb806020f4e1d756dec19b3e5e20e95e
Tyler Perron <tperron31313@gmail.com>
2022-11-28 14:08:20 -0500

    first commit

---
+++ FirstJavaProgram/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+	public static void main(String[] args) {
+		String name = "Coding Dojo";
+		int age = 100;
+		String hometown = "Burbank, CA";
+		System.out.printf("My name is %s \n", name);
+		System.out.printf("I am %d years old \n", age);
+		System.out.printf("My hometown is %s \n", hometown);
+		System.out.println("My name is" + name);
+		System.out.println("I am" + age + "years old");
+		System.out.println("My hometown is" + hometown);
+	}
+}
