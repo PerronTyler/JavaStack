fa2632b72d1f362ff5add585938c53f690ac5a00
Tyler Perron <tperron31313@gmail.com>
2022-12-09 15:48:41 -0500

    first commit

---
+++ travelers-with-login-red/src/main/java/com/tyler/travelers/TravelersWithLoginRedApplication.java
@@ -0,0 +1,13 @@
+package com.tyler.travelers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelersWithLoginRedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelersWithLoginRedApplication.class, args);
+	}
+
+}
